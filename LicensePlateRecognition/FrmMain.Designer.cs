02edc923a104fdcd53d29b0d4010f4712e48d8ab
La Quốc Thắng <quocthang0507@gmail.com>
2020-11-10 17:45:54 +0700

    Add project files.

---
+++ LicensePlateRecognition/FrmMain.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace LPR_Laptrinhvb
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.lblTotal = new System.Windows.Forms.Label();
+			this.lblNumber = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.btnOk = new System.Windows.Forms.Button();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.imageBox1 = new Emgu.CV.UI.ImageBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+			this.textBox1.Location = new System.Drawing.Point(24, 43);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(139, 68);
+			this.textBox1.TabIndex = 7;
+			this.textBox1.TabStop = false;
+			this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+			this.textBox2.Location = new System.Drawing.Point(24, 145);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(139, 26);
+			this.textBox2.TabIndex = 8;
+			this.textBox2.TabStop = false;
+			this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// lblTotal
+			// 
+			this.lblTotal.AutoSize = true;
+			this.lblTotal.BackColor = System.Drawing.Color.Transparent;
+			this.lblTotal.ForeColor = System.Drawing.Color.Black;
+			this.lblTotal.Location = new System.Drawing.Point(6, 127);
+			this.lblTotal.Name = "lblTotal";
+			this.lblTotal.Size = new System.Drawing.Size(175, 13);
+			this.lblTotal.TabIndex = 10;
+			this.lblTotal.Text = "Tổng số chữ nhận dạng được";
+			// 
+			// lblNumber
+			// 
+			this.lblNumber.AutoSize = true;
+			this.lblNumber.ForeColor = System.Drawing.Color.Black;
+			this.lblNumber.Location = new System.Drawing.Point(13, 27);
+			this.lblNumber.Name = "lblNumber";
+			this.lblNumber.Size = new System.Drawing.Size(160, 13);
+			this.lblNumber.TabIndex = 11;
+			this.lblNumber.Text = "Biển Số sau khi nhận dạng";
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(327, 290);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(277, 219);
+			this.panel1.TabIndex = 13;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.panel1);
+			this.groupBox1.Controls.Add(this.pictureBox3);
+			this.groupBox1.Controls.Add(this.label4);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.imageBox1);
+			this.groupBox1.Controls.Add(this.pictureBox1);
+			this.groupBox1.Controls.Add(this.pictureBox2);
+			this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.groupBox1.Location = new System.Drawing.Point(5, 7);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(619, 518);
+			this.groupBox1.TabIndex = 14;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Quá trình nhận dạng biển số xe";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.ForeColor = System.Drawing.Color.RoyalBlue;
+			this.label4.Location = new System.Drawing.Point(320, 271);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(177, 13);
+			this.label4.TabIndex = 17;
+			this.label4.Text = "4 - Những số nhận dạng được";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.ForeColor = System.Drawing.Color.RoyalBlue;
+			this.label3.Location = new System.Drawing.Point(9, 271);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(137, 13);
+			this.label3.TabIndex = 16;
+			this.label3.Text = "3 - Xử lý để nhận dạng";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.ForeColor = System.Drawing.Color.RoyalBlue;
+			this.label2.Location = new System.Drawing.Point(320, 24);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(133, 13);
+			this.label2.TabIndex = 15;
+			this.label2.Text = "2 - Nhận dạng biển số";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
+			this.label1.Location = new System.Drawing.Point(9, 24);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(77, 13);
+			this.label1.TabIndex = 14;
+			this.label1.Text = "1 - Hình gốc";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.linkLabel1);
+			this.groupBox2.Controls.Add(this.label5);
+			this.groupBox2.Controls.Add(this.btnOk);
+			this.groupBox2.Controls.Add(this.textBox1);
+			this.groupBox2.Controls.Add(this.lblNumber);
+			this.groupBox2.Controls.Add(this.textBox2);
+			this.groupBox2.Controls.Add(this.lblTotal);
+			this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.groupBox2.ForeColor = System.Drawing.Color.Red;
+			this.groupBox2.Location = new System.Drawing.Point(630, 7);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(191, 518);
+			this.groupBox2.TabIndex = 15;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Kết quả";
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.ForeColor = System.Drawing.Color.Black;
+			this.label5.Image = global::LPR_Laptrinhvb.Properties.Resources.info16;
+			this.label5.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+			this.label5.Location = new System.Drawing.Point(6, 225);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(185, 225);
+			this.label5.TabIndex = 12;
+			this.label5.Text = resources.GetString("label5.Text");
+			// 
+			// btnOk
+			// 
+			this.btnOk.ForeColor = System.Drawing.Color.Red;
+			this.btnOk.Image = global::LPR_Laptrinhvb.Properties.Resources.ActiveWindows32;
+			this.btnOk.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.btnOk.Location = new System.Drawing.Point(6, 177);
+			this.btnOk.Name = "btnOk";
+			this.btnOk.Size = new System.Drawing.Size(177, 36);
+			this.btnOk.TabIndex = 3;
+			this.btnOk.Text = "Chọn hình và\r\n nhận dạng";
+			this.btnOk.Click += new System.EventHandler(this.metroButton2_Click);
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox3.Location = new System.Drawing.Point(323, 287);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(286, 225);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox3.TabIndex = 18;
+			this.pictureBox3.TabStop = false;
+			// 
+			// imageBox1
+			// 
+			this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.imageBox1.Location = new System.Drawing.Point(11, 41);
+			this.imageBox1.Name = "imageBox1";
+			this.imageBox1.Size = new System.Drawing.Size(286, 225);
+			this.imageBox1.TabIndex = 2;
+			this.imageBox1.TabStop = false;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox1.Location = new System.Drawing.Point(323, 42);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(286, 225);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 5;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox2.Location = new System.Drawing.Point(11, 287);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(286, 225);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 6;
+			this.pictureBox2.TabStop = false;
+			// 
+			// linkLabel1
+			// 
+			this.linkLabel1.AutoSize = true;
+			this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+			this.linkLabel1.Location = new System.Drawing.Point(3, 497);
+			this.linkLabel1.Name = "linkLabel1";
+			this.linkLabel1.Size = new System.Drawing.Size(186, 17);
+			this.linkLabel1.TabIndex = 13;
+			this.linkLabel1.TabStop = true;
+			this.linkLabel1.Text = "http://www.laptrinhvb.net";
+			this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+			// 
+			// FrmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(825, 532);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "FrmMain";
+			this.ShowIcon = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "LAPTRINHVB.NET - Nhan dang bien so xe - Version 1.0";
+			this.Load += new System.EventHandler(this.FrmMain_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+		private System.Windows.Forms.Button btnOk;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label lblTotal;
+		private System.Windows.Forms.Label lblNumber;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ LicensePlateRecognition/FrmMain.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.ML;
+using Emgu.CV.ML.Structure;
+using Emgu.CV.UI;
+using Emgu.Util;
+using System.Diagnostics;
+using Emgu.CV.CvEnum;
+using System.IO;
+using System.Linq;
+using tesseract;
+using System.Collections;
+using LaptrinhVBLibs;
+using System.Reflection;
+
+namespace LPR_Laptrinhvb
+{
+	public partial class FrmMain : Form
+	{
+
+		public FrmMain()
+		{
+			InitializeComponent();
+		}
+
+		#region định nghĩa
+		List<Image<Bgr, Byte>> PlateImagesList = new List<Image<Bgr, byte>>();
+		List<string> PlateTextList = new List<string>();
+		List<Rectangle> listRect = new List<Rectangle>();
+		PictureBox[] box = new PictureBox[12];
+
+		public TesseractProcessor full_tesseract = null;
+		public TesseractProcessor ch_tesseract = null;
+		public TesseractProcessor num_tesseract = null;
+		private string m_path;
+		private List<string> lstimages = new List<string>();
+		private const string m_lang = "eng";
+		private string _Caption = "http://www.laptrinhvb.net";
+		#endregion
+
+		private void FrmMain_Load(object sender, EventArgs e)
+		{
+			m_path = GetAbsolutePath(typeof(Program).Assembly, "../../data/");
+			full_tesseract = new TesseractProcessor();
+			bool succeed = full_tesseract.Init(m_path, m_lang, 3);
+			if (!succeed)
+			{
+				MessageBox.Show("Lỗi thư viện Tesseract. Chương trình cần kết thúc.", _Caption);
+				Application.Exit();
+			}
+			full_tesseract.SetVariable("tessedit_char_whitelist", "ACDFHKLMNPRSTVXY1234567890").ToString();
+
+			ch_tesseract = new TesseractProcessor();
+			succeed = ch_tesseract.Init(m_path, m_lang, 3);
+			if (!succeed)
+			{
+				MessageBox.Show("Lỗi thư viện Tesseract. Chương trình cần kết thúc.", _Caption);
+				Application.Exit();
+			}
+			ch_tesseract.SetVariable("tessedit_char_whitelist", "ACDEFHKLMNPRSTUVXY").ToString();
+
+			num_tesseract = new TesseractProcessor();
+			succeed = num_tesseract.Init(m_path, m_lang, 3);
+			if (!succeed)
+			{
+				MessageBox.Show("Lỗi thư viện Tesseract. Chương trình cần kết thúc.", _Caption);
+				Application.Exit();
+			}
+			num_tesseract.SetVariable("tessedit_char_whitelist", "1234567890").ToString();
+
+			for (int i = 0; i < box.Length; i++)
+			{
+				box[i] = new PictureBox();
+			}
+			string folder = GetAbsolutePath(typeof(Program).Assembly, "../../ImageTest");
+			foreach (string fileName in Directory.GetFiles(folder, "*.bmp", SearchOption.TopDirectoryOnly))
+			{
+				lstimages.Add(Path.GetFullPath(fileName));
+			}
+			foreach (string fileName in Directory.GetFiles(folder, "*.jpg", SearchOption.TopDirectoryOnly))
+			{
+				lstimages.Add(Path.GetFullPath(fileName));
+			}
+		}
+
+		private void metroButton1_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void metroButton2_Click(object sender, EventArgs e)
+		{
+
+			OpenFileDialog dlg = new OpenFileDialog();
+			dlg.Filter = "Image (*.bmp; *.jpg; *.jpeg; *.png) |*.bmp; *.jpg; *.jpeg; *.png|All files (*.*)|*.*||";
+			dlg.InitialDirectory = GetAbsolutePath(typeof(Program).Assembly, "../../ImageTest");
+			if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.Cancel)
+			{
+				return;
+			}
+			string startupPath = dlg.FileName;
+
+			ProcessImage(startupPath);
+			if (PlateImagesList.Count != 0)
+			{
+				Image<Bgr, byte> src = new Image<Bgr, byte>(PlateImagesList[0].ToBitmap());
+				Bitmap grayframe;
+				Bitmap color;
+				int c = clsBSoft.IdentifyContours(src.ToBitmap(), 50, false, out grayframe, out color, out listRect);
+				pictureBox1.Image = color;
+				pictureBox2.Image = grayframe;
+				textBox2.Text = c.ToString();
+				Image<Gray, byte> dst = new Image<Gray, byte>(grayframe);
+				grayframe = dst.ToBitmap();
+				string zz = "";
+
+				// lọc và sắp xếp số
+				List<Bitmap> bmp = new List<Bitmap>();
+				List<int> erode = new List<int>();
+				List<Rectangle> up = new List<Rectangle>();
+				List<Rectangle> dow = new List<Rectangle>();
+				int up_y = 0, dow_y = 0;
+				bool flag_up = false;
+
+				int di = 0;
+
+				if (listRect == null) return;
+
+				for (int i = 0; i < listRect.Count; i++)
+				{
+					Bitmap ch = grayframe.Clone(listRect[i], grayframe.PixelFormat);
+					int cou = 0;
+					full_tesseract.Clear();
+					full_tesseract.ClearAdaptiveClassifier();
+					string temp = full_tesseract.Apply(ch);
+					while (temp.Length > 3)
+					{
+						Image<Gray, byte> temp2 = new Image<Gray, byte>(ch);
+						temp2 = temp2.Erode(2);
+						ch = temp2.ToBitmap();
+						full_tesseract.Clear();
+						full_tesseract.ClearAdaptiveClassifier();
+						temp = full_tesseract.Apply(ch);
+						cou++;
+						if (cou > 10)
+						{
+							listRect.RemoveAt(i);
+							i--;
+							di = 0;
+							break;
+						}
+						di = cou;
+					}
+				}
+
+				for (int i = 0; i < listRect.Count; i++)
+				{
+					for (int j = i; j < listRect.Count; j++)
+					{
+						if (listRect[i].Y > listRect[j].Y + 100)
+						{
+							flag_up = true;
+							up_y = listRect[j].Y;
+							dow_y = listRect[i].Y;
+							break;
+						}
+						else if (listRect[j].Y > listRect[i].Y + 100)
+						{
+							flag_up = true;
+							up_y = listRect[i].Y;
+							dow_y = listRect[j].Y;
+							break;
+						}
+						if (flag_up == true) break;
+					}
+				}
+
+				for (int i = 0; i < listRect.Count; i++)
+				{
+					if (listRect[i].Y < up_y + 50 && listRect[i].Y > up_y - 50)
+					{
+						up.Add(listRect[i]);
+					}
+					else if (listRect[i].Y < dow_y + 50 && listRect[i].Y > dow_y - 50)
+					{
+						dow.Add(listRect[i]);
+					}
+				}
+
+				if (flag_up == false) dow = listRect;
+
+				for (int i = 0; i < up.Count; i++)
+				{
+					for (int j = i; j < up.Count; j++)
+					{
+						if (up[i].X > up[j].X)
+						{
+							Rectangle w = up[i];
+							up[i] = up[j];
+							up[j] = w;
+						}
+					}
+				}
+				for (int i = 0; i < dow.Count; i++)
+				{
+					for (int j = i; j < dow.Count; j++)
+					{
+						if (dow[i].X > dow[j].X)
+						{
+							Rectangle w = dow[i];
+							dow[i] = dow[j];
+							dow[j] = w;
+						}
+					}
+				}
+
+				int x = 0;
+				int c_x = 0;
+
+				for (int i = 0; i < up.Count; i++)
+				{
+					Bitmap ch = grayframe.Clone(up[i], grayframe.PixelFormat);
+					Bitmap o = ch;
+					string temp;
+					if (i < 2)
+					{
+						temp = clsBSoft.Ocr(ch, false, full_tesseract, num_tesseract, ch_tesseract, true); // nhan dien so
+					}
+					else
+					{
+						temp = clsBSoft.Ocr(ch, false, full_tesseract, num_tesseract, ch_tesseract, false); // nhan dien chu
+					}
+
+					zz += temp;
+					box[i].Location = new Point(x + i * 50, 0);
+					box[i].Size = new Size(50, 100);
+					box[i].SizeMode = PictureBoxSizeMode.StretchImage;
+					box[i].Image = ch;
+					panel1.Controls.Add(box[i]);
+					c_x++;
+				}
+				zz += "\r\n";
+				for (int i = 0; i < dow.Count; i++)
+				{
+					Bitmap ch = grayframe.Clone(dow[i], grayframe.PixelFormat);
+					string temp = clsBSoft.Ocr(ch, false, full_tesseract, num_tesseract, ch_tesseract, true); // nhan dien so
+
+					zz += temp;
+					box[i + c_x].Location = new Point(x + i * 50, 100);
+					box[i + c_x].Size = new Size(50, 100);
+					box[i + c_x].SizeMode = PictureBoxSizeMode.StretchImage;
+					box[i + c_x].Image = ch;
+					panel1.Controls.Add(box[i + c_x]);
+				}
+				textBox1.Text = zz;
+
+			}
+
+		}
+
+		public void ProcessImage(string urlImage)
+		{
+			PlateImagesList.Clear();
+			PlateTextList.Clear();
+			panel1.Controls.Clear();
+			Bitmap img = new Bitmap(urlImage);
+			pictureBox2.Image = null;
+			pictureBox1.Image = img;
+			pictureBox1.Update();
+			clsBSoft.FindLicensePlate(img, pictureBox1, imageBox1, PlateImagesList, panel1);
+		}
+
+		private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			System.Diagnostics.Process.Start(linkLabel1.Text);
+		}
+
+		private string GetAbsolutePath(Assembly assembly, string relativePath)
+		{
+			var assemblyFolderPath = new FileInfo(assembly.Location).Directory.FullName;
+			return Path.GetFullPath(Path.Combine(assemblyFolderPath, relativePath));
+		}
+	}
+
+}
